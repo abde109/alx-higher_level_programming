987b29fb4422c205c93adcb0c52e6133f3ba8f28
abde109 <abderrahimkhadri2@gmail.com>
2023-08-12 03:17:55 +0100

    tesk 2 status

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
 
-argc = len(argv) - 1
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(argc))
+if __name__ == "__main__":
+    """Prints the number of and the list of its arguments."""
+    from sys import argv
 
-for i in range(1, argc + 1):
-    print("{}: {}".format(i, argv[i]))
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
