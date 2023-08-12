0d12f12757d4e8ad3ca5b6a1652dbbb09224a0ba
abde109 <abderrahimkhadri2@gmail.com>
2023-08-12 03:41:56 +0100

    tesk 4 status

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3.8
 
 if __name__ == "__main__":
-    """Prints all the names defined by the compiled module hidden_4.pyc (please download it locally)."""
     import hidden_4
 
     names = [name for name in dir(hidden_4) if name[:2] != '__']
