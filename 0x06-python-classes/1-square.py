37f7865b9687e98d0e87686dfa13127ef519e47e
abde109 <abderrahimkhadri2@gmail.com>
2023-08-30 23:34:18 +0100

    add for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""This module defines a class Square with a private attribute size."""
+
 
 class Square:
+    """A class that defines a square with a private attribute size."""
     def __init__(self, size):
         self.__size = size
