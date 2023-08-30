e470ed9cee1a5a6c6d33b3bf45d4a624be33888a
abde109 <abderrahimkhadri2@gmail.com>
2023-08-30 23:38:39 +0100

    add for task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -3,9 +3,49 @@
 
 
 class Square:
-    # (Same as Task 6, but add the following method)
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(n, int) for n in value) or \
+           not all(n >= 0 for n in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        print("\n" * self.__position[1], end="")
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
     def __str__(self):
-        """Returns a string representation of the square."""
-        if self.size == 0:
+        if self.__size == 0:
             return ""
-        return "\n".join([" " * self.position[0] + "#" * self.size for _ in range(self.size)])
+        result = "\n" * self.__position[1]
+        for i in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size + "\n"
+        return result
