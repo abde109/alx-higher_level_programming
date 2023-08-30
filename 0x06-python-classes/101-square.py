85b8b506e0715e560a8fe1e77a8222d967fc74c6
abde109 <abderrahimkhadri2@gmail.com>
2023-08-30 23:42:09 +0100

    add for task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -3,16 +3,21 @@
 
 
 class Square:
+    """This class defines a square with size and position."""
+    
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square with a size and position."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -21,10 +26,12 @@ class Square:
 
     @property
     def position(self):
+        """Retrieve the position of the square."""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Set the position of the square."""
         if not isinstance(value, tuple) or len(value) != 2 or \
            not all(isinstance(n, int) for n in value) or \
            not all(n >= 0 for n in value):
@@ -32,9 +39,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Calculate the area of the square."""
         return self.__size ** 2
 
     def my_print(self):
+        """Print the square."""
         if self.__size == 0:
             print()
             return
@@ -43,9 +52,12 @@ class Square:
             print(" " * self.__position[0] + "#" * self.__size)
 
     def __str__(self):
+        """Return a string representation of the square."""
         if self.__size == 0:
             return ""
         result = "\n" * self.__position[1]
         for i in range(self.__size):
-            result += " " * self.__position[0] + "#" * self.__size + "\n"
+            result += " " * self.__position[0] + "#" * self.__size
+            if i < self.__size - 1:
+                result += "\n"
         return result
