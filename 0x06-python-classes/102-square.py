3b83c19ce236da5be29e226a801794a5a6754f50
abde109 <abderrahimkhadri2@gmail.com>
2023-08-30 23:48:48 +0100

    add for task 9

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,23 +1,51 @@
 #!/usr/bin/python3
-"""This module defines a class Square that can be compared."""
+"""This module defines a class Square with comparators."""
 
 
 class Square:
-    # (Same as Task 4, but add the following methods)
+    """This class defines a square with a size that can be compared."""
+    def __init__(self, size=0):
+        """Initialize the square with a size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
     def __eq__(self, other):
+        """Compare if two squares are equal."""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """Compare if two squares are not equal."""
         return self.area() != other.area()
 
     def __lt__(self, other):
+        """Compare if one square is less than another."""
         return self.area() < other.area()
 
     def __le__(self, other):
+        """Compare if one square is less than or equal to another."""
         return self.area() <= other.area()
 
     def __gt__(self, other):
+        """Compare if one square is greater than another."""
         return self.area() > other.area()
 
     def __ge__(self, other):
+        """Compare if one square is greater than or equal to another."""
         return self.area() >= other.area()
