3b634de9d0c982fa37ead882e19e44e137ca30ed
abde109 <abderrahimkhadri2@gmail.com>
2023-09-05 00:03:00 +0100

    tasks status

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module that defines a rectangl"""
 
+
 class Rectangle:
     """A class that defines a rectangle by its width and height"""
     def __init__(self, width=0, height=0):
