99973ba8d42c1d7749c01c1b14d2914d5f3ad19f
abde109 <abderrahimkhadri2@gmail.com>
2023-09-09 03:18:41 +0100

    status task 0

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,10 +2,17 @@
 """This module defines a function that indents text based on specific characters."""
 
 def text_indentation(text):
-    """Print text with 2 new lines after ., ? and :."""
+    """Print a text with 2 new lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     
-    for char in ".:?":
-        text = text.replace(char, char + "\n\n")
-    print("\n".join(line.strip() for line in text.split("\n")))
+    line = ""
+    for char in text:
+        line += char
+        if char in ".?:":
+            print(line.strip())
+            print()
+            line = ""
+    if line:
+        print(line.strip())
+
