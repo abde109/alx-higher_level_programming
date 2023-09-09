962da03088ba17036bf006beada0f8ba886f9283
abde109 <abderrahimkhadri2@gmail.com>
2023-09-09 03:06:06 +0100

    status task 0

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,7 @@
 """This module defines a function that prints a given name."""
 
 def say_my_name(first_name, last_name=""):
-    """Print 'My name is <first name> <last name>'."""
+    """Print a name."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
