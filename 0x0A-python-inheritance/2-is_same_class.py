2dca9f176506d4fc93dc62a7fca3601e49525063
abde109 <abderrahimkhadri2@gmail.com>
2023-09-11 22:38:40 +0100

    first commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Check instance type """
+
+
+def is_same_class(obj, a_class):
+    """ Check if same class """
+    return type(obj) is a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Check if instance or inherited """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Check instance """
+    return isinstance(obj, a_class)
