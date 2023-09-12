ff2333e18ae4a6d2490a0ea320769390e03e7f45
abde109 <abderrahimkhadri2@gmail.com>
2023-09-13 00:35:25 +0100

    task 6

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Saves an object to a file in JSON format."""
-
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """Save an object as a JSON string in a file."""
