070cfa1e53ec669a13acc9069ec996e3693a3a65
abde109 <abderrahimkhadri2@gmail.com>
2023-09-13 00:49:45 +0100

    task 10 11 12

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a Student class with a method to retrieve filtered attributes."""
+
+
+class Student:
+    """Represents a student."""
+    
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a filtered dictionary description."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    filtered_dict[attr] = self.__dict__[attr]
+            return filtered_dict
---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a Student class with methods to serialize and deserialize instances."""
+
+
+class Student:
+    """Represents a student."""
+    
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a filtered dictionary description."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    filtered_dict[attr] = self.__dict__[attr]
+            return filtered_dict
+
+    def reload_from_json(self, json):
+        """Replace all attributes from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
