34248e8d7d76d0a3af196db043a6cf4f687791af
abde109 <abderrahimkhadri2@gmail.com>
2023-09-13 00:29:23 +0100

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Converts an object to a JSON string."""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Return JSON representation of an object."""
+    return json.dumps(my_obj)
