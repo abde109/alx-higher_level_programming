422690d42e9c879ff5bda58a01d6fba17bf15c0b
abde109 <abderrahimkhadri2@gmail.com>
2023-09-13 00:25:39 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Reads and prints a text file."""
+
+def read_file(filename=""):
+    """Read and print a text file (UTF-8)."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
