c5bcf785c00ae56d89037b47118cb1650ce19d22
abde109 <abderrahimkhadri2@gmail.com>
2023-10-18 00:53:30 +0100

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,8 @@
 -- 100-move_to_utf8.sql
+-- Use the hbtn_0c_0 database
+USE hbtn_0c_0;
+
 -- Converting database, table, and column to UTF8
-
--- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the name column to UTF8
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
