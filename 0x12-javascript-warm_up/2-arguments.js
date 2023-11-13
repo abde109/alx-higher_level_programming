e9b4b99e51602b8e9b609f8fc144be91ce2e6997
abde109 <abderrahimkhadri2@gmail.com>
2023-11-13 23:33:47 +0100

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.args.slice(2);
+if (args.length !== 0) {
+  console.log('No argument');
+} else if (args.length) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
