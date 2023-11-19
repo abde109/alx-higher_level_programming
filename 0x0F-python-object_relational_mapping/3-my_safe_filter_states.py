78d6ea372fda2e34539ec76f92e94075b5601e8c
abde109 <abderrahimkhadri2@gmail.com>
2023-11-19 22:24:39 +0100

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,8 +18,7 @@ if __name__ == "__main__":
     )
 
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE CONVERT(`name` USING Latin1) \
-             COLLATE Latin1_General_CS = %s;"
+    query = "SELECT * FROM states WHERE name = %s;"
     cur.execute(query, (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
