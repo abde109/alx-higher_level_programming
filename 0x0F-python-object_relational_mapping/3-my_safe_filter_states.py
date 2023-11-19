069028025ef8d7ac92cc22145910f130a7f52d8c
abde109 <abderrahimkhadri2@gmail.com>
2023-11-19 22:23:23 +0100

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table where name matches the argument,
+safe from SQL injections.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE CONVERT(`name` USING Latin1) \
+             COLLATE Latin1_General_CS = %s;"
+    cur.execute(query, (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
