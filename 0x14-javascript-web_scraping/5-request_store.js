5ce1c20202de12d8d00f23a040711fbce2fddeb2
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 20:07:29 +0000

    latest

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,19 +5,15 @@ const fs = require('fs');
 const api = process.argv[2];
 const file = process.argv[3];
 
-const writeToFile = (data) => {
-  fs.writeFile(file, data, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-};
-
 request(api, (err, response, body) => {
   if (err) {
     console.error(err);
   } else if (response.statusCode === 200) {
-    writeToFile(body);
+    fs.writeFile(file, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
   } else {
     console.log('code: ' + response.statusCode);
   }
