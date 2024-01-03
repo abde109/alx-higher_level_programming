a6e191fd03cac2eeca69dbebf36fb99c30f6621a
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 17:37:31 +0000

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function(error, response, body) {
+    if (error) {
+        console.error(error);
+    } else if (response.statusCode === 200){
+        fs.writeFile(file, body, 'utf-8', function(err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('File saved successfully!');
+            }
+        });
+    } else {
+        console.log('code: '+ response.statusCode);
+    }
+});
+
