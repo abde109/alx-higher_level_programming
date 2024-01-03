d450044eb0d5c1ec26eb0bf81c159444a8f62d6d
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 16:04:04 +0000

    latest

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,8 @@ const file = process.argv[2];
 
 fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
-        console.log(err);
+        return console.error(err);
     } else {
-        console.log(data)
+        console.log(data);
     }
 });
