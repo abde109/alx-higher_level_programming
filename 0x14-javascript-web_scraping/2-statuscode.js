d4754ff7842bd3290004d03b75e25796de19de6f
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 16:52:40 +0000

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (response) => {
-  console.log('code: ' + response.statusCode);
+request.get(url, (response, err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
