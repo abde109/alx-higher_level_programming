16ecf0072da42a2446a2f3c3c1c4d0a7c53c2a86
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 17:53:43 +0000

    latest

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const api = 'https://swapi.dev/api/films/' + process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    characters.forEach(function (characterUrl) {
+      request(characterUrl, function (error, response, body) {
+        if (error) {
+          console.error(error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
