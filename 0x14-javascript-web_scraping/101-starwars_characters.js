cc1826233f8606fdf4c49e058b9bcb9fd7258c46
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 20:30:20 +0000

    latest

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,21 +3,21 @@
 const request = require('request');
 const api = 'https://swapi.dev/api/films/' + process.argv[2];
 
-function fetch(api) {
-    return new Promise((resolve, reject) => {
-        request(api, { json: true }, (err, response, body) => {
-            if (err) reject(err);
-            resolve(body);
-        });
+function fetch (api) {
+  return new Promise((resolve, reject) => {
+    request(api, { json: true }, (err, response, body) => {
+      if (err) reject(err);
+      resolve(body);
     });
+  });
 }
 
 fetch(api).then(movie => {
-    const character = movie.characters.map(Url => fetch(Url));
+  const character = movie.characters.map(Url => fetch(Url));
 
-    character.reduce((prev, curr) => {
-        return prev.then(() => curr.then(character => {
-            console.log(character.name);
-        }));
-    }, Promise.resolve());
+  character.reduce((prev, curr) => {
+    return prev.then(() => curr.then(character => {
+      console.log(character.name);
+    }));
+  }, Promise.resolve());
 }).catch(err => console.error(err));
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request.get(process.argv[2]).on('response',(response, err)=> {
+request.get(process.argv[2]).on('response', (response, err) => {
   if (err) {
     console.log(err);
   } else {
