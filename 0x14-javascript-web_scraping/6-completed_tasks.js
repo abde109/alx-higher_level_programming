3a33e0e57585e0f8e0fdec2ff56237b84ffa4017
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 17:50:54 +0000

    latest

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,21 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function(err, response, body) {
-    if(err){
-        console.error(err);
-    }else if (response.statusCode === 200){
-        const todos = JSON.parse(body);
-        const completed = {};
-        todos.forEach(todo => {
-            if (todo.completed === true) {
-                if (completed[todo.userId] === undefined) {
-                    completed[todo.userId] = 1;
-                } else {
-                    completed[todo.userId] += 1;
-                }
-            }
-        });
-        console.log(completed);
-}});
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach(todo => {
+      if (todo.completed === true) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId] += 1;
+        }
+      }
+    });
+    console.log(completed);
+  }
+});
