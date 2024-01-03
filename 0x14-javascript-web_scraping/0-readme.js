e5a4a8af1deab8a216751640279dc702e73352cb
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 15:59:47 +0000

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf8', function (err, data) {
+fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
         console.log(err);
     } else {
-        console.log(data + "\n")
+        console.log(data)
     }
 });
