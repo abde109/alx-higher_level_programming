f3098eb9a2455f1fd60c14d17b0e1d71755cd8ef
abde109 <abderrahimkhadri2@gmail.com>
2024-01-03 20:23:01 +0000

    latest

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const api = 'https://swapi.dev/api/films/' + process.argv[2];
+
+function fetch(api) {
+    return new Promise((resolve, reject) => {
+        request(api, { json: true }, (err, response, body) => {
+            if (err) reject(err);
+            resolve(body);
+        });
+    });
+}
+
+fetch(api).then(movie => {
+    console.log(movie);
+    const character = movie.characters.map(Url => fetch(Url));
+
+    character.reduce((prev, curr) => {
+        return prev.then(() => curr.then(character => {
+            console.log(character.name);
+        }));
+    }, Promise.resolve());
+}).catch(err => console.error(err));
