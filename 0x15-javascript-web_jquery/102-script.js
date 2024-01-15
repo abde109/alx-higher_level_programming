1695512855cbc9c03c173ea8fe8e8920dd7ad322
abde109 <abderrahimkhadri2@gmail.com>
2024-01-15 16:05:31 +0000

    all tasks

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#btn_translate').click(function() {
+        let lang = $('#language_code').val();
+        $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`, function(data) {
+            $('#hello').text(data.hello);
+        });
+    });
+});
